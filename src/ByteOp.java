2d8cedc76ecbcd334fa2951921b33c73c050f1c1
ryco218 <ryco218@yahoo.com>
2012-02-15 14:53:15 -0500

    Adding ByteOp class

---
+++ src/ByteOp.java
@@ -0,0 +1,47 @@
+public class ByteOp {
+
+        // takes 16 bytes from wav
+        public static byte[] encode(byte msgByte, byte[] wavBytes, boolean littleEndian) {
+                int offset = 0;
+                byte[] newBytes = new byte[8];
+                for (int i = 0; i < 8; i++) {
+                        newBytes[i] = (byte)(msgByte << i >> 7);
+                        newBytes[i] |= (byte)(-2);
+                }
+                if(!littleEndian){
+                        offset = 1;
+                }
+
+                for (int i = 0; i < 8; i++) {
+                        wavBytes[offset] &= newBytes[i];
+
+                        offset += 2;
+                }
+
+                return wavBytes;
+        }
+
+        // takes 16 bytes from wav
+        public static byte decode(byte[] wavBytes, boolean littleEndian) {
+                int offset = 0;
+                byte returnedByte = 0;
+
+                if(!littleEndian){
+                        offset = 1;
+                }
+
+                for (int i = 0; i < 8; i++) {
+                        byte temp = (byte)(wavBytes[offset] << 7 >> i);
+                        int mask = 2^(7 - i);
+                        temp &= mask;
+                        returnedByte |= temp;
+                        offset += 2;
+                }
+
+                return returnedByte;
+
+
+        }
+
+
+}
